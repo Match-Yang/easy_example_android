153bcadc635e3a4ce8a40c37c771a57d57975535
alan <alanliu@zego.im>
2022-05-13 21:28:44 +0800

    seperate HttpClient

--- app/src/main/java/im/zego/easyexample/android/express/AppCenter.java
+++ app/src/main/java/im/zego/easyexample/android/express/AppCenter.java
@@ -5,9 +5,4 @@ public class AppCenter {
     // Get your AppID from ZEGOCLOUD Console
     //[My Projects] : https://console.zegocloud.com/project
     public static long appID = ;
-
-    // Get your ServerSecret from ZEGOCLOUD Console
-    // [My Projects -> project's Edit -> Server Secret Keys] : https://console.zegocloud.com/project"
-    public static String serverSecret = ;
-
 }
